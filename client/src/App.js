3debca907016c297f19019db4121c2beebfeccc0
thais17 <thai@zen8labs.com>
2020-05-08 16:26:30 +0700

    setting up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavBar'
+import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap'
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -7,9 +13,15 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <AppNavBar/>
-            </div>
+            <Provider store={store}>
+                <div className="App">
+                    <AppNavBar/>
+                    <Container>
+                        <ItemModal/>
+                        <ShoppingList/>
+                    </Container>
+                </div>
+            </Provider>
         );
     }
 }
